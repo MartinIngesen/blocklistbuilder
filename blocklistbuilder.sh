3cf8493f1068e900bd47d18727ac9a4c62123098
Mrtn <martin@ingesen.no>
2016-07-14 22:42:53 +0000

    added new custom blocklist because gist is shit

--- blocklistbuilder.sh
+++ blocklistbuilder.sh
@@ -30,7 +30,7 @@ echo "Getting Mother of All Ad Blocks list..."
 curl -A 'Mozilla/5.0 (X11; Linux x86_64; rv:30.0) Gecko/20100101 Firefox/30.0' -e http://forum.xda-developers.com/ http://adblock.mahakala.is/ | grep -v "#" | awk '{print $2}' | sort >> $tempoutlist
 
 echo "Getting custom block list"
-curl -s https://gist.githubusercontent.com/MartinIngesen/73df5cf99654dd227e581f2cd79f4e93/raw/605278be04a1724a137bf1c834f558316aff2a26/customblocklist | sort >> $tempoutlist
+curl -s https://raw.githubusercontent.com/MartinIngesen/blocklistbuilder/master/customblocklist.txt | sort >> $tempoutlist
 
 echo "writing temp file"
 cat $tempoutlist | sed $'s/\r$//' | sort -u | sed '/^$/d' | awk -v "IP=$destinationIP" '{sub(/\r$/,""); print "address=/"$0"/"IP}' > $outlist
